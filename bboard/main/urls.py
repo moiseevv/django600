68ec131e025fd02feec3e071af78775ef633a9e1
Vladimir Moiseev neo <lutii@ngs.ru>
2022-06-29 23:28:18 +0700

    new html

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
+from .views import BBLoginView
+from .views import BBLogoutView
+from .views import profile
 
-from .views import index
+from .views import index, other_page
 
 app_name = 'main'
 urlpatterns = [
-    path('', index, name='index')
+    path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
+    path('accounts/profile/', profile, name='profile'),
+    path('accounts/login/', BBLoginView.as_view(), name = 'login'),
+    path('<str:page>/',other_page, name='other'),
+    path('', index, name='index'),
 ]
--- bboard/main/views.py
+++ bboard/main/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.template.loader import get_template
+from django.template import TemplateDoesNotExist
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+
+class BBLogoutView(LoginRequiredMixin, LogoutView):
+    template_name = 'main/logout.html'
+
+class BBLoginView(LoginView):
+    template_name = 'main/login.html'
 
 def index(requerst):
     return render(requerst, 'main/index.html')
+
+def other_page(request, page):
+    try:
+        template = get_template('main/'+page+'.html')
+    except TemplateDoesNotExist:
+        raise Http404
+    return HttpResponse(template.render(request=request))
+
+@login_required
+def profile(request):
+    return render(request, 'main/profile.html')
