379f0ce55083819f3991f5f038b54a8edffe6aab
vmoiseev <vmoiseev@ro-nso.ru>
2022-07-01 18:55:58 +0700

    page change fio

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .views import BBLoginView
 from .views import BBLogoutView
 from .views import profile
+from .views import ChangeUserInfoView
 
 from .views import index, other_page
 
 app_name = 'main'
 urlpatterns = [
+    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name = 'profile_change'),
     path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
     path('accounts/profile/', profile, name='profile'),
     path('accounts/login/', BBLoginView.as_view(), name = 'login'),
--- bboard/main/views.py
+++ bboard/main/views.py
@@ -6,6 +6,30 @@ from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import UpdateView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
+
+from .models import AdvUser
+from .forms import ChangeUserInfoForm
+
+class ChangeUserInfoView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
+    model = AdvUser
+    template_name = 'main/change_user_info.html'
+    form_class = ChangeUserInfoForm
+    success_url = reverse_lazy('main:profile')
+    success_message = 'Данные пользователя изменены'
+
+    def setup(self, request, *args, **kwargs):
+        self.user_id = request.user.pk
+        return super().setup(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+        return get_object_or_404(queryset, pk=self.user_id)
+
 
 class BBLogoutView(LoginRequiredMixin, LogoutView):
     template_name = 'main/logout.html'
